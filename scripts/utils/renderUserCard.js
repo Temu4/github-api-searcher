fafa08701c1caf70cda726c809af8761507808f2
Roman Tsuhulov <84708720+Sigul4@users.noreply.github.com>
2022-06-17 19:07:01 +0300

    Add files via upload

--- scripts/utils/renderUserCard.js
+++ scripts/utils/renderUserCard.js
@@ -9,10 +9,10 @@ const renderUserCard = (user) => {
   const {avatar_url, login, url} = user;
 
   return `<li class="list__item">
-              <a class="card" href="${url}">
+              <div class="card">
                 <img class="card__img" src="${avatar_url}" alt="${login}" />
                 <p class="card__name">${login}</p>
-              </a>
+              </div>
             </li>`;
 };
 
